347406376def34a68774dd213e52016814e0445c
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-01-21 14:32:45 -0500

    Controller page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//This is my CONTROLLER
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//define a default route(home page)
+$f3->route('GET /', function() {
+    //echo "Hello";
+    $view = new Template();
+    echo $view->render('/views/home.html');
+});
+//run fat free
+$f3->run();
